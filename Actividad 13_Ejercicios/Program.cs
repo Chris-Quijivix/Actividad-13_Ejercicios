74fb4826d6ac62434f81f933b18ce546b96f0340
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-17 08:46:41 -0600

    Avances 1

--- Actividad 13_Ejercicios/Program.cs
+++ Actividad 13_Ejercicios/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿class Estudiante
+{
+    public string Nombre;
+    public double Nota1;
+    public double Nota2;
+    public double Nota3;
+
+    public double Promedio()
+    {
+        return (Nota1 +  Nota2 + Nota3) / 3;
+    }
+
+
+}
